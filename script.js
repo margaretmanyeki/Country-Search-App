d71ad6a6734f86ab0e6290f945c69eef5e00e3f5
margaret manyeki <maggiemanyeki6@gmail.com>
2022-06-22 14:18:04 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInput = document.getElementById("country-input");
+searchBtn.addEventListener("click",()=>{
+    let countryName = "India";
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    console.log(finalURL);
+    fetch(finalURL).then((res)=>res.json())
+    .then((data) =>{
+console.log(data[0].capital[0]);
+console.log(data[0].flag.svg);
+console.log(data[0].name.common);
+console.log(data[0].continents[0]);
+console.log(Object.keys(data[0].currencies)[0]);
+console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+console.log(Object.values(data[0].languages).toString().split(",").join(","));
+
+    result.innerHTML  = `<img src="${data[0].flags.svg}"class="flag-img">
+    `;
+
+
+
+    
+    });
+});
+
